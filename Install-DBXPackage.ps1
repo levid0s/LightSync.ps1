78e2e74f1b461947cb9709723f574d8ee0a3f006
lev@levidos.com <lev@levidos.com>
2023-01-04 00:29:37 +0000

    Added support for reg

--- Install-DBXPackage.ps1
+++ Install-DBXPackage.ps1
@@ -11,7 +11,6 @@ $VerbosePreference = 'Continue'
 
 ${Dry-Run} = $false
 
-$DBXRoot = "N:\Tools"
 
 function Install-ModuleIfNotPresent {
   param(
@@ -85,29 +84,35 @@ function Add-UserPaths {
   }
 }
 
+$DropboxRealRoot = "D:\Dropbox"
+$DBXRoot = "N:\Tools"
+$StartMenu = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\DBXSync"
+$StartUp = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+
+if (!(Test-Path "$StartUp\subst-m-n.bat")) {
+  $formatText = @"
+  subst m: $DropboxRealRoot\music
+  subst n: $DropboxRealRoot
+  pause
+"@
+  Set-Content  "$StartUp\subst-m-n.bat" -Value $formatText
+  $Subst
+}
+
 
 $PackageObj = Get-Content $PackageFile | ConvertFrom-Yaml
 $PackageName = [System.IO.Path]::GetFileNameWithoutExtension($PackageFile)
 $PackageNameDSC = 'DSC-' + $PackageName -replace '[^0-9a-zA-Z-]', ''
 
 Install-ModuleIfNotPresent 'PowerShell-YAML'
-
 Install-ModuleIfNotPresent 'DSCR_Shortcut'
 Install-ModuleIfNotPresent 'DSCR_FileAssoc'
 
-if ($PackageObj.Shortcuts.Assoc) {
-  
-  $CredsNeeded = $true
-}
-else {
-  $CredsNeeded = $false
-}
+$CredsNeeded = $PackageObj.Shortcuts.Assoc -ne $true
 
 if ($CredsNeeded -and $PsDscRunAsCreds -eq $null) {
   [PSCredential]$PsDscRunAsCreds = Get-Credential
 }
-
-#$PsDscRunAsCreds = $env:PsDscRunAsCreds
 
 $Config = @{
   AllNodes = @(
@@ -118,8 +123,6 @@ $Config = @{
   )
 }
 
-$StartMenu = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\DBXSync"
-$StartUp = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 
 Configuration $PackageNameDSC {
   Import-DscResource -ModuleName DSCR_Shortcut
@@ -163,6 +166,20 @@ Configuration $PackageNameDSC {
       }    
     }
 
+    $i = 0
+    # foreach ($r in $PackageObj.reg) {
+    #   $robj ++
+    #   Registry $robj {
+    #     Key       = $r.key
+    #     ValueName = $r.valuename
+    #     ValueData = TemplateStr($r.valuedata)
+    #     ValueType = $r.valuetype
+    #     Force     = $true
+    #     PsDscRunAsCredential = ($PsDscRunAsCreds)
+    #     Ensure    = "Present"  # You can also set Ensure to "Absent"
+    #   }      
+    # }
+
     # $pObj = 1
     # foreach ($p in $PackageObj.Paths) {
     #   xEnvironment $pObj {
@@ -190,8 +207,19 @@ Start-DscConfiguration -Wait -Verbose -Path "./mofs/$PackageNameDSC"
 ###  Do the paths manually
 ###
 
-if($PackageObj.Paths) {
+if ($PackageObj.Paths) {
   $PathsStr = TemplateStr($PackageObj.Paths -join ';')
   $PathsList = $PathsStr -split ';'
   Add-UserPaths($PathsList)  
+}
+
+###
+###  Do regs manually
+###
+
+foreach ($r in $PackageObj.reg) {
+  $r.key = $r.key -replace 'HKEY_CURRENT_USER\\', 'HKCU:\' -replace 'HKEY_LOCAL_MACHINE\\', 'HKLM:\'
+  if (!$r.name) { $r.Name = '(Default)' }
+  if (!$r.type) { $r.Type = 'String' }
+  New-ItemProperty -Path $r.key -Name $r.name -Value (TemplateStr($r.data))  -PropertyType $r.type -Verbose -Force
 }
